476547eb176b04dd1fafb7d2e4a51bea6af33b52
izzy <mista.rol@gmail.com>
2023-11-30 21:42:50 +0700

    dark mode toggle, basic components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import { ThemeProvider } from "@/components/theme-provider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <html lang="en" suppressHydrationWarning></html>
-      <head />
-      <main className={GeistSans.className}>
+      <main className={GeistMono.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { Button } from '@/components/ui/button'
+import { ModeToggle } from '@/components/ui/mode-toggle'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col items-center p-24`}>
+    <main className={`flex min-h-screen flex-col gap-2 items-center p-24`}>
       <div>asdf lol</div>
       <div>again and again</div>
+      <Button>Lorem ipsum</Button>
+      <ModeToggle/>
     </main>
   )
 }
