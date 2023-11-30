a004853d7674cef5ad7d4356e5ff8e1d5ca3e5f9
izzy <mista.rol@gmail.com>
2023-12-01 00:28:32 +0700

    hero input field setup

---
+++ components/ui/input-hero.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const InputHero = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const input = event.target as HTMLInputElement;
+      input.value = input.value.slice(0, 3);
+    };
+
+    return (
+      <input
+        type="number"
+        maxLength={3}
+        className={cn(
+          "flex h-auto w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 text-7xl",
+          className
+        )}
+        onInput={handleInput}
+        {...props}
+      />
+    );
+  }
+);
+InputHero.displayName = "Input";
+
+export { InputHero };
