a86e5c64b9689a6e4f27c5f66e05f10b3f07710a
izzy <mista.rol@gmail.com>
2023-12-01 03:49:32 +0700

    ui cleanup, banner setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
 import React, { useState, useEffect, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { InputBpmFps } from "@/components/ui/input-bpm-fps";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 import DynamicTitle from "@/components/TitleDynamic";
 import FourBars from "@/components/FourBars";
 
@@ -84,7 +86,7 @@ export default function Home() {
   const handleFpsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFps(Number(event.target.value));
   };
-  
+
   const fourBarsRef = useRef<{ resetBeat: () => void }>(null);
 
   const handleReset = () => {
@@ -95,86 +97,93 @@ export default function Home() {
 
   return (
     <>
-    <DynamicTitle bpm={bpm} />
-    <main
-      className={`flex min-h-screen flex-col gap-2 items-center p-24 font-semibold`}
-    >
-      <div>asdf lol</div>
-      <div>again and again</div>
-      <ModeToggle />
+      <DynamicTitle bpm={bpm} />
+      <main
+        className={`flex min-h-screen flex-col gap-2 items-center p-0 font-semibold`}
+      >
 
-      <div className="text-2xl mt-4">
-        <FourBars ref={fourBarsRef} bpm={bpm} />
-      </div>
-
-      {/* Tap and sync */}
-      <div className="p-4 flex gap-1 w-screen max-w-2xl justify-center">
-        <Button onClick={handleTap}>Tap</Button>
-      </div>
-      <div className="p-4 flex gap-1 w-screen max-w-2xl justify-center">
-        <Button onClick={handleReset}>Resync</Button>
-      </div>
-
-      {/* Input */}
-      <div className="p-4 flex gap-1 w-screen max-w-2xl justify-center mb-6">
-        <div className="flex flex-col gap-1">
-          <div className="h-1/2 p-1 flex flex-col justify-end">
-            Tempo
+        {/* Banner */}
+        <div className="p-4 flex gap-1 justify-between content-center w-screen">
+          <div className="align-bottom text-4xl font-extrabold pt-1">Pulse</div>
+          <div className="align-bottom text-4xl font-extrabold pt-0"><ModeToggle /></div>
+        </div>
+        <Separator className="mb-6" />
+        
+        {/* Tap and sync */}
+        <div className="text-2xl mt-4 mb-6">
+          <FourBars ref={fourBarsRef} bpm={bpm} />
+        </div>
+        <div className="p-0 flex gap-20 w-screen max-w-2xl justify-center mb-6">
+          <div className="p-1 flex gap-0 max-w-2xl justify-center">
+            <Button className="text-md p-6" onClick={handleTap}>
+              &nbsp;Tap&nbsp;
+            </Button>
           </div>
-          <div className="h-1/2 p-1 flex flex-col justify-end">
-            Frame rate &nbsp;&nbsp;
+          <div className="p-1 flex gap-1 max-w-2xl justify-center">
+            <Button className="text-md p-6" onClick={handleReset}>
+              Resync
+            </Button>
           </div>
         </div>
 
-        <div className="flex flex-col w-40 gap-1">
-          <InputBpmFps
-            inputMode="numeric"
-            type="number"
-            placeholder="128"
-            name="inputBpm"
-            value={bpm}
-            onChange={handleBpmChange}
-          />
-          <InputBpmFps
-            inputMode="numeric"
-            type="number"
-            placeholder="24"
-            name="inputFps"
-            defaultValue={fps}
-            onChange={handleFpsChange}
-          />
-        </div>
+        {/* Input */}
+        <div className="p-4 flex gap-1 w-screen max-w-2xl justify-center mb-6">
+          <div className="flex flex-col gap-1">
+            <div className="h-1/2 p-1 flex flex-col justify-end">Tempo</div>
+            <div className="h-1/2 p-1 flex flex-col justify-end">
+              Frame rate &nbsp;&nbsp;
+            </div>
+          </div>
 
-        <div className="flex flex-col gap-1">
-          <div className="h-1/2 p-1 flex flex-col justify-end">bpm</div>
-          <div className="h-1/2 p-1 flex flex-col justify-end">fps</div>
+          <div className="flex flex-col w-40 gap-1">
+            <InputBpmFps
+              inputMode="numeric"
+              type="number"
+              placeholder="128"
+              name="inputBpm"
+              value={bpm}
+              onChange={handleBpmChange}
+            />
+            <InputBpmFps
+              inputMode="numeric"
+              type="number"
+              placeholder="24"
+              name="inputFps"
+              defaultValue={fps}
+              onChange={handleFpsChange}
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <div className="h-1/2 p-1 flex flex-col justify-end">bpm</div>
+            <div className="h-1/2 p-1 flex flex-col justify-end">fps</div>
+          </div>
         </div>
-      </div>
 
         <Separator className="mb-6" />
 
-      {/* Output */}
-      <div className="p-4 flex gap-1 w-screen max-w-2xl justify-center font-light italic text-sm">
-        <div className="flex flex-col w-1/2">
-          <div className="h-1/2 p-1 flex flex-col justify-end">
-            Frames per beat
+        {/* Output */}
+        <div className="p-4 flex gap-1 w-screen max-w-2xl justify-center font-light italic text-sm">
+          <div className="flex flex-col w-1/2">
+            <div className="h-1/2 p-1 flex flex-col justify-end">
+              Frames per beat
+            </div>
+            <div className="h-1/2 p-1 flex flex-col justify-end">
+              Frames per bar &#40;4/4&#41;
+            </div>
           </div>
-          <div className="h-1/2 p-1 flex flex-col justify-end">
-            Frames per bar &#40;4/4&#41;
+          <div className="flex flex-col">
+            <div className="h-1/2 p-1 flex flex-col justify-end">
+              {/* // The result for frames per beat should be rendered here, everytime the input is changed */}
+              {framesPerBeat}
+            </div>
+            <div className="h-1/2 p-1 flex flex-col justify-end">
+              {/* // The result for frames per bar (four beats) should be rendered here, everytime the input is changed */}
+              {framesPerBar}
+            </div>
           </div>
         </div>
-        <div className="flex flex-col">
-          <div className="h-1/2 p-1 flex flex-col justify-end">
-            {/* // The result for frames per beat should be rendered here, everytime the input is changed */}
-            {framesPerBeat}
-          </div>
-          <div className="h-1/2 p-1 flex flex-col justify-end">
-            {/* // The result for frames per bar (four beats) should be rendered here, everytime the input is changed */}
-            {framesPerBar}
-          </div>
-        </div>
-      </div>
-    </main>
+      </main>
     </>
   );
 }
