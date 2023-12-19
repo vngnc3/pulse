97b08cb654591e0de3bad7f2e0c5c7f589aecfbc
izzy <mista.rol@gmail.com>
2023-12-19 23:42:33 +0700

    calculate overflow frames, use table component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
-import Image from "next/image";
 import React, { useState, useEffect, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { InputBpmFps } from "@/components/ui/input-bpm-fps";
-import { AspectRatio } from "@/components/ui/aspect-ratio";
 import DynamicTitle from "@/components/TitleDynamic";
 import FourBars from "@/components/FourBars";
+import { Table, TableRow, TableBody, TableCell } from "@/components/ui/table";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 function calculateFramesPerBeat(bpm: number, fps: number): number {
   // Check for invalid input and log an error
@@ -22,9 +27,28 @@ function calculateFramesPerBeat(bpm: number, fps: number): number {
   const framesPerBeat = fps * secondsPerBeat;
 
   // Return the rounded number of frames per beat
-  return Math.round(framesPerBeat);
+  return framesPerBeat;
 
   // TODO: Calculate real frames per beat, then build a logic to recommend frames pulldown
+}
+
+function calculateOverflowFrames(rawfpb: number) {
+  // rawfpb is actual frames per beat
+  // grab the overflow or underflow frames
+  // inform the user when to add (or remove) an extra frame after every X frames
+  // this technique is known as pulldown in video format
+
+  let roundedFpb = Math.round(rawfpb);
+  let overflow = rawfpb - roundedFpb;
+  if (overflow === 0) {
+    return 0;
+  }
+  let pulldown = Math.floor(1 / overflow);
+  if (pulldown === Infinity) {
+    return 0;
+  } else {
+    return pulldown;
+  }
 }
 
 export default function Home() {
@@ -101,14 +125,15 @@ export default function Home() {
       <main
         className={`flex min-h-screen flex-col gap-2 items-center p-0 font-semibold`}
       >
-
         {/* Banner */}
         <div className="p-4 flex gap-1 justify-between content-center w-screen">
           <div className="align-bottom text-4xl font-extrabold pt-1">Pulse</div>
-          <div className="align-bottom text-4xl font-extrabold pt-0"><ModeToggle /></div>
+          <div className="align-bottom text-4xl font-extrabold pt-0">
+            <ModeToggle />
+          </div>
         </div>
         <Separator className="mb-6" />
-        
+
         {/* Tap and sync */}
         <div className="text-2xl mt-4 mb-6">
           <FourBars ref={fourBarsRef} bpm={bpm} />
@@ -160,28 +185,61 @@ export default function Home() {
           </div>
         </div>
 
-        <Separator className="mb-6" />
+        <Separator className="mb-1" />
 
         {/* Output */}
-        <div className="p-4 flex gap-1 w-screen max-w-2xl justify-center font-light italic text-sm">
-          <div className="flex flex-col w-1/2">
-            <div className="h-1/2 p-1 flex flex-col justify-end">
-              Frames per beat
-            </div>
-            <div className="h-1/2 p-1 flex flex-col justify-end">
-              Frames per bar &#40;4/4&#41;
-            </div>
-          </div>
-          <div className="flex flex-col">
-            <div className="h-1/2 p-1 flex flex-col justify-end">
-              {/* // The result for frames per beat should be rendered here, everytime the input is changed */}
-              {framesPerBeat}
-            </div>
-            <div className="h-1/2 p-1 flex flex-col justify-end">
-              {/* // The result for frames per bar (four beats) should be rendered here, everytime the input is changed */}
-              {framesPerBar}
-            </div>
-          </div>
+
+        <div className="p-4 flex gap-1 w-96 justify-center font-light text-sm">
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">Frames/beat</TableCell>
+                <TableCell className="text-right">
+                  {Math.round(framesPerBeat)}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="font-medium">Frames/bar</TableCell>
+                <TableCell className="text-right">
+                  {Math.round(framesPerBar)}
+                </TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="font-medium">
+                  Actual frames/beat
+                </TableCell>
+                <TableCell className="text-right">{framesPerBeat}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell className="font-medium">
+                  <TooltipProvider>
+                    <Tooltip>
+                      <TooltipTrigger>Overflow frames❔</TooltipTrigger>
+                      <TooltipContent>
+                        <p>
+                          {calculateOverflowFrames(framesPerBeat) > 0
+                            ? "Add "
+                            : "Remove "}
+                          1 frame after every{" "}
+                          {Math.abs(calculateOverflowFrames(framesPerBeat))} frames to
+                          nudge the animation back in sync with the tempo.
+                        </p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </TableCell>
+                <TableCell className="text-right">
+                  {calculateOverflowFrames(framesPerBeat) === 0
+                    ? "No overflow ✨"
+                    : calculateOverflowFrames(framesPerBeat)}
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </div>
+        <Separator className="mb-6" />
+        <div className="text-xs font-light opacity-20 p-6 pb-20">
+          Made by izzy at Bay Team
         </div>
       </main>
     </>
