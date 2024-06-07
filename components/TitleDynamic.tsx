cd7a58b02dc92f257bde534db3d492bfd8e7397f
izzy <mista.rol@gmail.com>
2024-06-07 14:58:50 +0700

    og image

--- components/TitleDynamic.tsx
+++ components/TitleDynamic.tsx
@@ -1,16 +1,23 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 const appTitle = "Pulse";
+const ogImagePath = "/pulse-og.png";
 
 interface DynamicTitleProps {
   bpm: number;
 }
 
 const DynamicTitle = ({ bpm }: DynamicTitleProps) => {
-
   return (
     <Head>
       <title>{`${appTitle} | ${bpm} bpm`}</title>
+      <meta property="og:title" content="Pulse" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://pulse.vngnc.xyz/" />
+      <meta
+        property="og:image"
+        content="https://pulse.vngnc.xyz/pulse-og.png"
+      />
     </Head>
   );
 };
